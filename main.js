4f33fbcf3bc5fe771a7faaf6d4008bc35c8e67fd
Gustaf Nilklint <gustaf.nilklint@jayway.com>
2018-01-11 23:11:39 +0100

    trying to add an alternative main file

---
+++ main.js
@@ -0,0 +1,33 @@
+var bleno = require('bleno');
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var Throttle = require('./characteristics/throttle');
+var Steering = require('./characteristics/steering');
+
+console.log('bleno - echo');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('RCCar', ['00000000-0000-1000-8000-00805F9B34F0']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: '00000000-0000-1000-8000-00805F9B34F0',
+        characteristics: [
+          new Throttle(),
+          new Steering()
+        ]
+      })
+    ]);
+  }
+});

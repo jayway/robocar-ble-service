3617c375176c5927c6c7d173d0364c98b27365d1
Gustaf Nilklint <gustaf.nilklint@jayway.com>
2018-01-11 21:00:43 +0100

    fixed ReferenceError

--- RCCarService.js
+++ RCCarService.js
@@ -9,8 +9,8 @@ function RCCarService() {
   bleno.PrimaryService.call(this, {
     uuid: '00000000-0000-1000-8000-00805F9B34F0',
     characteristics: [
-      new ThrottleCharacteristic(),
-      new SteeringCharacteristic()
+      new Throttle(),
+      new Steering()
     ]
   });
 };
